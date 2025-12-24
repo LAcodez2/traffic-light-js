a01a79574ee6bd7c213738209c56b78ad391c546
Albert Valderas <albertvalderas052101@gmail.com>
2025-12-24 15:31:04 -0600

    Complete traffic light assignment

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -1,17 +1,40 @@
 import { Component } from "react";
 
+const lights = ["red", "green", "yellow"];
+
 export class ClassTrafficLight extends Component {
+  state = {
+    lightIndex: 0,
+  };
+
+  nextState = () => {
+    this.setState((prevState) => ({
+      lightIndex: (prevState.lightIndex + 1) % lights.length,
+    }));
+  };
+
   render() {
+    const currentLight = lights[this.state.lightIndex];
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
+
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div
+            className={`circle ${currentLight === "red" ? "red" : "black"}`}
+          ></div>
+          <div
+            className={`circle ${
+              currentLight === "yellow" ? "yellow" : "black"
+            }`}
+          ></div>
+          <div
+            className={`circle ${currentLight === "green" ? "green" : "black"}`}
+          ></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.nextState}>
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,17 +1,26 @@
+const lights = ["red", "green", "yellow"];
+
 export const FunctionalTrafficLight = () => {
+  const [index, setIndex] = useState(0);
+
+  const nextState = () => {
+    setIndex((prev) => (prev + 1) % lights.length);
+  };
+
+  const current = lights[index];
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
-      <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
-      </div>
-      <button className="next-state-button">Next State</button>
+      <div className={`light red ${current === "red" ? "active" : ""}`}></div>
+      <div
+        className={`light yellow ${current === "yellow" ? "active" : ""}`}
+      ></div>
+      <div
+        className={`light green ${current === "green" ? "active" : ""}`}
+      ></div>
+
+      <button onClick={nextState}>Next State</button>
     </div>
   );
 };
-
-
-
